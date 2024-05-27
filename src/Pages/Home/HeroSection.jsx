8a444120f4a333f5dc338a94043dc26fb251e73a
Iliyan Alibhai <ialib001@ucr.edu>
2024-05-27 10:17:22 -0700

    finished css for herosec

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -18,7 +18,7 @@ export default function HeroSection() {
         <button className="btn btn-primary">Get In Touch</button>
       </div>
       <div className="hero--section--image">
-        <img src="./img/hero_img.png" alt="Hero Section" />
+        <img src="./img/hero_image.png" alt="Hero Section" className="hero_img"/>
       </div>
     </section>
   );
